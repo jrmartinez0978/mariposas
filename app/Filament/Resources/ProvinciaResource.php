c39e24fbff42e4d117fb9e458f5ac86e9d0c5408
jrmartinez0978 <jrmartinez0978@gmail.com>
2024-01-29 14:31:14 -0500

    creando categorias para el panel

---
+++ app/Filament/Resources/ProvinciaResource.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\ProvinciaResource\Pages;
+use App\Filament\Resources\ProvinciaResource\RelationManagers;
+use App\Models\Provincia;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class ProvinciaResource extends Resource
+{
+    protected static ?string $model = Provincia::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                //
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                //
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListProvincias::route('/'),
+            'create' => Pages\CreateProvincia::route('/create'),
+            'edit' => Pages\EditProvincia::route('/{record}/edit'),
+        ];
+    }
+}
