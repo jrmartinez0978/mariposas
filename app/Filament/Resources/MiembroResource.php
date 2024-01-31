143037d80344881749f0354ef07c9c7d4dae22ce
jrmartinez0978 <jrmartinez0978@gmail.com>
2024-01-31 05:40:09 -0500

    cambio iconos

--- app/Filament/Resources/MiembroResource.php
+++ app/Filament/Resources/MiembroResource.php
@@ -22,7 +22,7 @@ class MiembroResource extends Resource
 {
     protected static ?string $model = Miembro::class;
 
-    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+    protected static ?string $navigationIcon = 'heroicon-o-user-plus';
 
     public static function form(Form $form): Form
     {
--- app/Filament/Resources/MunicipioResource.php
+++ app/Filament/Resources/MunicipioResource.php
@@ -19,7 +19,7 @@ class MunicipioResource extends Resource
 {
     protected static ?string $model = Municipio::class;
 
-    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+    protected static ?string $navigationIcon = 'heroicon-o-map-pin';
 
     public static function form(Form $form): Form
     {
