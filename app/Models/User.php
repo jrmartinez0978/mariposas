047aa4bb11cb522ec062f15813777121eccead3a
jrmartinez0978 <jrmartinez0978@gmail.com>
2024-01-31 21:58:17 -0500

    correcion email

--- app/Models/User.php
+++ app/Models/User.php
@@ -53,6 +53,6 @@ class User extends Authenticatable implements FilamentUser
 
     public function canAccessPanel(Panel $panel): bool
     {
-        return str_ends_with($this->email, '@gmail.com') && $this->hasVerifiedEmail();
+        return $this->hasVerifiedEmail();
     }
 }
